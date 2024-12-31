8f05c303e5ecebd17dc5d3f5196d6868f2571623
GaneshGhatti <ganeshghatti6@gmail.com>
2024-12-31 18:50:12 +0530

    configure .env

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const connectdatabase = async () => {
   try {
