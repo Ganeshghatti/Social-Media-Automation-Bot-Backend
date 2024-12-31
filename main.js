6a6ce8621852545b4d03ce4f38163f6e0cd12094
GaneshGhatti <ganeshghatti6@gmail.com>
2024-12-31 18:53:40 +0530

    configure .env

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-
+const path = require('path');
 // Set default NODE_ENV if not set
 const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
 dotenv.config({ path: path.resolve(process.cwd(), envFile) });
